32f4e7c03c31df41c5a0b705301d36207fb032c9
Jaeha Choi <104782252+wogk9854@users.noreply.github.com>
2023-01-26 13:49:08 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.Collections;
+class Solution {
+    public int solution(int []A, int []B) {
+        int answer = 0;
+            Arrays.sort(A);
+            Arrays.sort(B);
+            for (int i = 0, j = B.length-1; i < A.length; i++,j--) {
+                answer += A[i] * B[j];
+            }
+            return answer;
+        }
+    }
