de84c890b54d5b21cff2ac0c6131195791c3b8cd
Jaeha Choi <104782252+wogk9854@users.noreply.github.com>
2023-03-04 23:52:03 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.03 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        double result = ((double)num1 / (double)num2) * 1000;
+        int answer = (int)result;
+        return answer;
+    }
+}
